d7ff5cc3e19bf95825db10b7069c72e796522895
YUVASRIANANDHAN <yuva1482005@gmail.com>
2025-07-22 21:06:15 +0530

    string Or number palindrome

---
+++ palindrome.py
@@ -0,0 +1,6 @@
+string=input("Enter a string: ")
+reversed_string = string[::-1]
+if (reversed_string == string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")    
