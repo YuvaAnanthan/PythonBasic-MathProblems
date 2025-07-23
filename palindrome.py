07b24c6b95be55b6d32502c11df9bd82d786a626
YUVASRIANANDHAN <yuva1482005@gmail.com>
2025-07-23 21:31:13 +0530

    string Or number palindrome

--- palindrome.py
+++ palindrome.py
@@ -5,3 +5,13 @@ if (reversed_string == string):
     print("The string is a palindrome.")
 else:
     print("The string is not a palindrome.")    
+
+#method 2:using number
+def num_palindrome(n):
+    return str(n) == str(n)[::-1]
+   
+num = int(input("Enter a number: "))
+if num_palindrome(num):
+    print(f" {num} The number is a palindrome.")
+else:
+    print(f"{num} The number is not a palindrome.")    
